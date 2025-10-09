124328c177357b8ce177a9041c7a25c2c78d5e1a
Zeshan Ahmad <zahmad5870@gmail.com>
2025-10-09 05:35:01 -0400

    finished added icons and placing them for left tab

--- src/components/leftTab/LeftTab.tsx
+++ src/components/leftTab/LeftTab.tsx
@@ -1,5 +1,24 @@
+import searchIcon from "../../assets/icons/search.svg";
+import sourceControlIcon from "../../assets/icons/sourcecontrol.svg";
+import explorerIcon from "../../assets/icons/explorer.svg";
+import debugIcon from "../../assets/icons/debug.svg";
+import extensionIcon from "../../assets/icons/extensions.svg";
+import accountIcon from "../../assets/icons/account.svg";
+import settingIcon from "../../assets/icons/setting.svg";
+
+import styles from "./LeftTab.module.css";
 const LeftTab = () => {
-  return <div>Left</div>;
+  return (
+    <div className={styles.imgContainer}>
+      <img src={explorerIcon} className={styles.imgExplorer}></img>
+      <img src={searchIcon} className={styles.imgSearch}></img>
+      <img src={sourceControlIcon} className={styles.imgSourceControl}></img>
+      <img src={debugIcon} className={styles.img}></img>
+      <img src={extensionIcon} className={styles.img}></img>
+      <img src={accountIcon} className={styles.imgAccount}></img>
+      <img src={settingIcon} className={styles.img}></img>
+    </div>
+  );
 };
 
 export default LeftTab;
