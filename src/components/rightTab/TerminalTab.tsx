fa2a7d0c7e818d3e1296ebec570dd0dd34c50e6d
Zeshan Ahmad <zahmad5870@gmail.com>
2025-10-10 22:36:34 -0400

    quick vercel deployment fix

--- src/components/rightTab/TerminalTab.tsx
+++ src/components/rightTab/TerminalTab.tsx
@@ -7,5 +7,15 @@ import terminal from "../../assets/terminal/terminal.svg";
 import trash from "../../assets/terminal/trash.svg";
 
 export default function TerminalTab() {
-  return <div>Terminal Tab</div>;
+  return (
+    <div>
+      <img src={chevrondo} />
+      <img src={add} />
+      <img src={close} />
+      <img src={ellipsis} />
+      <img src={splithorizontal} />
+      <img src={terminal} />
+      <img src={trash} />
+    </div>
+  );
 }
