92312af74f9ca76e734181f969ec6c785e12dde4
Zeshan Ahmad <zahmad5870@gmail.com>
2025-10-13 20:01:02 -0400

    almost got middle tab working

--- src/components/middleTab/MiddleTab.tsx
+++ src/components/middleTab/MiddleTab.tsx
@@ -1,5 +1,129 @@
 import styles from "./MiddleTab.module.css";
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
+interface FolderType {
+  folderName?: string;
+  folderOpen?: boolean;
+  folderContent?: (string | FolderType)[];
+  fileName?: string;
+}
+
+const fileStructure: FolderType[] = [
+  {
+    folderName: "node_modules",
+    folderOpen: false,
+    folderContent: ["react", "typescript"],
+  },
+  {
+    folderName: "src",
+    folderOpen: false,
+    folderContent: [
+      {
+        folderName: "assets",
+        folderOpen: false,
+        folderContent: ["image1", "image2", "image3"],
+      },
+      {
+        folderName: "components",
+        folderOpen: false,
+        folderContent: [
+          { fileName: "leftTab.tsx" },
+          { fileName: "middleTab.tsx" },
+          { fileName: "rightTab.tsx" },
+        ],
+      },
+      {
+        folderName: "screens",
+        folderOpen: false,
+        folderContent: [
+          { fileName: "HomeScreen.tsx" },
+          { fileName: "AboutScreen.tsx" },
+          { fileName: "ResumeScreen.tsx" },
+        ],
+      },
+      {
+        folderName: "utils",
+        folderOpen: false,
+        folderContent: [
+          {
+            folderName: "contexts",
+            folderOpen: false,
+            folderContent: [{ fileName: "FileHighlightContext.ts" }],
+          },
+        ],
+      },
+      {
+        fileName: "App.tsx",
+      },
+    ],
+  },
+  {
+    fileName: "index.html",
+  },
+  {
+    fileName: "package-lock.json",
+  },
+  {
+    fileName: "package.json",
+  },
+  {
+    fileName: "README.md",
+  },
+  {
+    fileName: "tsconfig.json",
+  },
+];
+
+const RenderFolderStructure: React.FC<{ folder: FolderType }> = ({
+  folder,
+}) => {
+  const [folderStructure, setFolderStructure] = useState(folder);
+  //lets map the array and return an opened and unopened state for each folderName field
+
+  // const dropDownNormalizer = (index) => {
+  //   setFolderStructure((prev)=> prev.map((item, i)=> i !== index ? (): item))
+  // };
+
+  return (
+    <div>
+      {folderStructure.folderName && (
+        <p style={{ fontSize: 14 }}>
+          {" "}
+          {`>`} {folderStructure.folderName}
+        </p>
+      )}
+      {folderStructure.folderOpen === true ? (
+        <p style={{ fontSize: 14 }}> {folderStructure.fileName}</p>
+      ) : (
+        folderStructure.folderContent?.map((item) => {
+          if (typeof item === "string") {
+            return (
+              <div key={item}>
+                <p style={{ fontSize: 10 }}>{item}</p>
+              </div>
+            );
+          } else {
+            return (
+              <RenderFolderStructure key={item.folderName} folder={item} />
+            );
+          }
+        })
+      )}
+      {/* {folderStructure.folderContent?.map((item) => {
+        if (typeof item === "string") {
+          return (
+            <div key={item}>
+              <p style={{ fontSize: 10 }}>{item}</p>
+            </div>
+          );
+        } else {
+          return <RenderFolderStructure key={item.folderName} folder={item} />;
+        }
+      })} */}
+    </div>
+  );
+};
+
 export default function MiddleTab() {
   const [middleWidth, setMiddleWidth] = useState<number>(200);
 
@@ -41,6 +165,11 @@ export default function MiddleTab() {
     <div className={styles.middleTabContainer} ref={containerRef}>
       <div className={styles.middleContent} style={{ width: middleWidth }}>
         Middle Tab
+        <div>
+          {fileStructure.map((item) => (
+            <RenderFolderStructure key={item.folderName} folder={item} />
+          ))}
+        </div>
       </div>
       <div className={styles.resizeHandle}></div>
     </div>
