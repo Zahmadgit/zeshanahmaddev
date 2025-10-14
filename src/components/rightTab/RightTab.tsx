376e57d78d637857e295ba6f9b2af5391a0c3e67
Zeshan Ahmad <zahmad5870@gmail.com>
2025-10-14 14:21:17 -0400

    gave up on being smart about placing the numbers dynamically

--- src/components/rightTab/RightTab.tsx
+++ src/components/rightTab/RightTab.tsx
@@ -3,11 +3,16 @@ import styles from "./RightTab.module.css";
 import { useState, useRef, useEffect } from "react";
 import useCurrentFileState from "../../store/useCurrentFileState";
 
+const lorem =
+  "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.";
+
 export default function RightTab() {
   const [terminalHeight, setTerminalHeight] = useState<number>(200);
   const fileState = useCurrentFileState((state) => state.currentFile);
   const changeFile = useCurrentFileState((state) => state.changeFile);
   const containerRef = useRef<HTMLDivElement>(null);
+
+  const rightTabNumAmount = useRef(45);
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       //ref needs to exist
@@ -16,6 +21,7 @@ export default function RightTab() {
       }
       const rectangleSize = containerRef.current.getBoundingClientRect();
       const newHeight = rectangleSize.bottom - e.clientY;
+
       if (newHeight > 100 && newHeight < rectangleSize.height * 0.7) {
         setTerminalHeight(newHeight);
       }
@@ -42,21 +48,28 @@ export default function RightTab() {
   }, []);
 
   const columnNumbers = [];
-  for (let i = 1; i < 100; i++) {
+  for (let i = 1; i < rightTabNumAmount.current; i++) {
     columnNumbers.push(i);
   }
   return (
     <div className={styles.rightTabContainer} ref={containerRef}>
-      <div style={{ flex: 1, flexDirection: "row", display: "flex" }}>
-        <div>
+      <div
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          display: "flex",
+        }}
+      >
+        <div style={{ maxHeight: 10, textOverflow: "clip" }}>
           {columnNumbers.map((number) => (
             <div key={number}>
-              <p>{number}</p>
+              <p className={styles.numbers}>{number}</p>
             </div>
           ))}
         </div>
+        <div className={styles.numberAndTextDivider}></div>
         <div className={styles.codebarContainer}>
-          Right
+          {lorem}
           <p>{fileState}</p>
           <button
             type="button"
