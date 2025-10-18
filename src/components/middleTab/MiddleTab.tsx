da9d0e1762bad9b6f743a0078ab5c7d6d4ecdd20
Zeshan Ahmad <zahmad5870@gmail.com>
2025-10-18 19:49:05 -0400

    simplifying middle and terminal tab/removing tab extension func

--- src/components/middleTab/MiddleTab.tsx
+++ src/components/middleTab/MiddleTab.tsx
@@ -1,50 +1,50 @@
 import styles from "./MiddleTab.module.css";
-import { useState, useRef, useEffect } from "react";
+// import { useRef } from "react";
 
 import ExplorerZeshanDev from "./ExplorerZeshanDev";
 
 export default function MiddleTab() {
-  const [middleWidth, setMiddleWidth] = useState<number>(170);
+  // const [middleWidth, setMiddleWidth] = useState<number>(170);
 
-  const containerRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      //ref needs to exist
-      if (!containerRef.current) {
-        return;
-      }
-      const rectangleSize = containerRef.current.getBoundingClientRect();
-      const newWidth = e.clientX - rectangleSize.left;
-      if (newWidth > 150 && newWidth < 500) {
-        setMiddleWidth(newWidth);
-      }
-    };
+  // const containerRef = useRef<HTMLDivElement>(null);
+  // useEffect(() => {
+  //   const handleMouseMove = (e: MouseEvent) => {
+  //     //ref needs to exist
+  //     if (!containerRef.current) {
+  //       return;
+  //     }
+  //     const rectangleSize = containerRef.current.getBoundingClientRect();
+  //     const newWidth = e.clientX - rectangleSize.left;
+  //     if (newWidth > 150 && newWidth < 500) {
+  //       setMiddleWidth(newWidth);
+  //     }
+  //   };
 
-    const handleMouseUp = () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-    };
+  //   const handleMouseUp = () => {
+  //     document.removeEventListener("mousemove", handleMouseMove);
+  //     document.removeEventListener("mouseup", handleMouseUp);
+  //   };
 
-    const handleMouseDown = () => {
-      document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mouseup", handleMouseUp);
-    };
+  //   const handleMouseDown = () => {
+  //     document.addEventListener("mousemove", handleMouseMove);
+  //     document.addEventListener("mouseup", handleMouseUp);
+  //   };
 
-    const resizeHandle = containerRef.current?.querySelector(
-      `.${styles.resizeHandle}`
-    );
-    resizeHandle?.addEventListener("mousedown", handleMouseDown);
+  //   const resizeHandle = containerRef.current?.querySelector(
+  //     `.${styles.resizeHandle}`
+  //   );
+  //   resizeHandle?.addEventListener("mousedown", handleMouseDown);
 
-    return () => {
-      resizeHandle?.removeEventListener("mousedown", handleMouseDown);
-    };
-  }, []);
+  //   return () => {
+  //     resizeHandle?.removeEventListener("mousedown", handleMouseDown);
+  //   };
+  // }, []);
 
   return (
-    <div className={styles.middleTabContainer} ref={containerRef}>
-      <div className={styles.middleContent} style={{ width: middleWidth }}>
-        <ExplorerZeshanDev />
-      </div>
+    <div className={styles.middleTabContainer}>
+      {/* <div className={styles.middleContent} style={{ width: middleWidth }}> */}
+      <ExplorerZeshanDev />
+      {/* </div> */}
       <div className={styles.resizeHandle}></div>
     </div>
   );
--- src/components/rightTab/RightTab.tsx
+++ src/components/rightTab/RightTab.tsx
@@ -4,7 +4,34 @@ import { useState, useRef, useEffect } from "react";
 import useCurrentFileState from "../../store/useCurrentFileState";
 
 const lorem =
-  "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.";
+  "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibu" +
+  "s ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.";
+// "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum" +
+// "egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class " +
+// "aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos." +
+// "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibu" +
+// "s ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis." +
+// "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum" +
+// "egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class " +
+// "aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos." +
+// "s ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis." +
+// "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum" +
+// "egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class " +
+// "aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos." +
+// "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibu" +
+// "s ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis." +
+// "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum" +
+// "egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class " +
+// "aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos." +
+// "s ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis." +
+// "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum" +
+// "egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class " +
+// "aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos." +
+// "s ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis." +
+// "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum" +
+// "egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class " +
+// "aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos." +
+// "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibu";
 
 export default function RightTab() {
   const [terminalHeight, setTerminalHeight] = useState<number>(150);
@@ -13,39 +40,39 @@ export default function RightTab() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const rightTabNumAmount = useRef(45);
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      //ref needs to exist
-      if (!containerRef.current) {
-        return;
-      }
-      const rectangleSize = containerRef.current.getBoundingClientRect();
-      const newHeight = rectangleSize.bottom - e.clientY;
+  // useEffect(() => {
+  //   const handleMouseMove = (e: MouseEvent) => {
+  //     //ref needs to exist
+  //     if (!containerRef.current) {
+  //       return;
+  //     }
+  //     const rectangleSize = containerRef.current.getBoundingClientRect();
+  //     const newHeight = rectangleSize.bottom - e.clientY;
 
-      if (newHeight > 100 && newHeight < rectangleSize.height * 0.7) {
-        setTerminalHeight(newHeight);
-      }
-    };
+  //     if (newHeight > 100 && newHeight < rectangleSize.height * 0.7) {
+  //       setTerminalHeight(newHeight);
+  //     }
+  //   };
 
-    const handleMouseUp = () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-    };
+  //   const handleMouseUp = () => {
+  //     document.removeEventListener("mousemove", handleMouseMove);
+  //     document.removeEventListener("mouseup", handleMouseUp);
+  //   };
 
-    const handleMouseDown = () => {
-      document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mouseup", handleMouseUp);
-    };
+  //   const handleMouseDown = () => {
+  //     document.addEventListener("mousemove", handleMouseMove);
+  //     document.addEventListener("mouseup", handleMouseUp);
+  //   };
 
-    const resizeHandle = containerRef.current?.querySelector(
-      `.${styles.resizeHandle}`
-    );
-    resizeHandle?.addEventListener("mousedown", handleMouseDown);
+  //   const resizeHandle = containerRef.current?.querySelector(
+  //     `.${styles.resizeHandle}`
+  //   );
+  //   resizeHandle?.addEventListener("mousedown", handleMouseDown);
 
-    return () => {
-      resizeHandle?.removeEventListener("mousedown", handleMouseDown);
-    };
-  }, []);
+  //   return () => {
+  //     resizeHandle?.removeEventListener("mousedown", handleMouseDown);
+  //   };
+  // }, []);
 
   const columnNumbers = [];
   for (let i = 1; i < rightTabNumAmount.current; i++) {
@@ -68,9 +95,10 @@ export default function RightTab() {
           ))}
         </div>
         <div className={styles.numberAndTextDivider}></div>
+
         <div className={styles.codebarContainer}>
           {lorem}
-          <p>{fileState}</p>
+          {/* <p>{fileState}</p>
           <button
             type="button"
             onClick={() => {
@@ -78,10 +106,10 @@ export default function RightTab() {
             }}
           >
             Change the file State to Bruh
-          </button>
+          </button> */}
         </div>
       </div>
-      <div className={styles.resizeHandle}></div>
+      {/* <div className={styles.resizeHandle}></div> */}
       <div
         className={styles.terminalContainer}
         style={{ height: terminalHeight }}
